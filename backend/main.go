5ebec1401e563bbc05bec5a28aedab2d0c6973b1
Maxim Yefremov <maxYefr@gmail.com>
2018-03-23 10:44:22 +0300

    hello play docker,main.go, makefile

---
+++ backend/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	cmd := exec.Command("flite", "-t", os.Args[1], "-o", "output.wav")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+}
