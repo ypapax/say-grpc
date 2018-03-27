8062907b04c2296125749004d83913f32d3fd919
Maxim Yefremov <maxYefr@gmail.com>
2018-03-27 14:59:32 +0300

    text as first arg

--- say/main.go
+++ say/main.go
@@ -9,13 +9,21 @@ import (
 	pb "github.com/ypapax/say-grpc/api"
 	"context"
 	"io/ioutil"
+	"os"
+	"fmt"
 )
 
 func main() {
 	backend := flag.String("b", "localhost:8080", "address of the say backend")
-	text := flag.String("t", "hello", "string to say")
 	output := flag.String("o", "output.wav", "wav output file")
 	flag.Parse()
+
+	if len(os.Args) < 2 {
+		fmt.Printf("usage:\n\t%s \"text to speak\"\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	text := os.Args[1]
 
 	conn, err := grpc.Dial(*backend, grpc.WithInsecure())
 	if err != nil {
@@ -24,9 +32,9 @@ func main() {
 	defer conn.Close()
 
 	client := pb.NewTextToSpeechClient(conn)
-	res, err := client.Say(context.Background(), &pb.Text{Text: *text})
+	res, err := client.Say(context.Background(), &pb.Text{Text: text})
 	if err != nil {
-		log.Fatalf("could not say %s: %+v", *text, err)
+		log.Fatalf("could not say %s: %+v", text, err)
 	}
 	if err := ioutil.WriteFile(*output, res.Audio, 0666); err != nil {
 		log.Fatalf("could not write to file %s: %+v", *output, err)
